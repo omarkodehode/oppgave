a7b9b63b4dc3b26b39e981d94ed8123970bebb01
omarkodehode <aom022025@gmail.com>
2025-03-20 11:09:22 +0100

    Update Program.cs

--- oppgave/Program.cs
+++ oppgave/Program.cs
@@ -13,8 +13,8 @@ class Program
         
         int Temp = Convert.ToInt32 (Console.ReadLine());
        
-        // if (input != null && int.TryParse(input, out int Temp))
-        // {
+        if (input != null && int.TryParse(input, out int Temp))
+        {
 
 
 
@@ -57,10 +57,10 @@ class Program
                             break;
                     }
             }
-        // }
-        // else
-        // {Console.WriteLine("enter a valid number");
-        //     }
+     }
+         else
+        {Console.WriteLine("enter a valid number");
+        }
   
 
 
